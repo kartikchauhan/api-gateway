c9f6ec53650c9df2d70cf7c02c287247eac2b2fb
kartikchauhan <chauhan.kartik25@gmail.com>
2022-12-24 15:53:48 +0530

    import Router from middleware

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const { Router } = require('./middleware');
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(Router)
 
 app.listen(PORT, function() {
     console.log('Listening on port ' + PORT);
