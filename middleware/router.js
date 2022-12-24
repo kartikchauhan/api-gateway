b710e76d7e5e1c29fa17428a7952ea9f12c6219c
kartikchauhan <chauhan.kartik25@gmail.com>
2022-12-25 02:51:54 +0530

    get service info and add guard clauses

--- middleware/router.js
+++ middleware/router.js
@@ -1,13 +1,36 @@
+const { requestResolver } = require('./requestResolver')
+
 const Router = require('express').Router()
 
 Router.all('*', (req, res, next) => {
-    console.log('Inside router')
+    const services = requestResolver()
 
-    if (req.url === '/users' && req.method === 'GET') {
-        // const body = req.body
+    let port, baseUrl, endpoints
+    const reqMethod = req.method.toLowerCase()
 
-        // make http call to POST /users/
+    switch (req.url) {
+    case '/users': {
+        const serviceInfo = services.userService
+
+        port = serviceInfo.port
+        baseUrl = serviceInfo.base_url
+        endpoints = serviceInfo.endpoints
+
+        console.log(endpoints[reqMethod].includes('dsds'))
+
+        if (typeof endpoints[reqMethod] === 'undefined' || !endpoints[reqMethod].includes(req.url)) {
+            throw new Error('service info not found')
+        } else {
+            // make HTTP call to the endpoint
+        }
     }
+        break
+
+    default: {
+        throw new Error('service info not found')
+    }
+    }
+
     next()
 })
 
