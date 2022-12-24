27bcffeea41e5688d2c1f2a262642ba6e66fddfd
kartikchauhan <chauhan.kartik25@gmail.com>
2022-12-25 01:31:25 +0530

    add custom logging format

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const rfs = require('rotating-file-stream')
+const path = require('path')
 
-const { Router } = require('./middleware');
+const { Router } = require('./middleware')
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
-const app = express();
+const app = express()
 
-app.use(morgan('dev'));
-app.use(bodyParser.json());
+const logStream = rfs.createStream('access.log', {
+    interval: '1d',
+    path: path.join(__dirname, 'log')
+})
+
+/**
+ * log request in custom format:
+ * [24/Dec/2022:19:14:46 +0000] ::1 - PostmanRuntime/7.29.2 "GET /users HTTP/1.1" | 200 24.461
+ */
+morgan.format('custom', '[:date[clf]] :remote-addr :remote-user :user-agent ":method :url HTTP/:http-version" | :status :total-time')
+
+app.use(morgan('custom', {
+    stream: logStream
+}))
+app.use(morgan('dev'))
+app.use(bodyParser.json())
 app.use(Router)
 
-app.listen(PORT, function() {
-    console.log('Listening on port ' + PORT);
-});
+app.listen(PORT, function () {
+    console.log('Listening on port ' + PORT)
+})
